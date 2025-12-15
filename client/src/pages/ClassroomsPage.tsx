b14b57cdfd2a61ccb75ab30a064be8f578ca0688
Adrian Azor <Adrianazor0@hotmail.com>
2025-12-14 22:26:08 -0400

    Configurar CI/CD con GitHub Actions

--- client/src/pages/ClassroomsPage.tsx
+++ client/src/pages/ClassroomsPage.tsx
@@ -29,7 +29,7 @@ export const ClassroomsPage = () => {
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-bold text-gray-800">Aulas y Espacios</h1>
         <button onClick={() => setIsModalOpen(true)} className="bg-indigo-600 text-white px-4 py-2 rounded-lg flex gap-2">
-          <Plus size={20} /> Nueva Salon
+          <Plus size={20} /> Nueva Aula
         </button>
       </div>
 
